585d63856c4afc88ed3b98d0be27ff61fdcde2ea
Drew Powers <drew@pow.rs>
2019-06-02 18:22:37 -0600

    Add logging

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Manifold on ZEIT
+
+Preview app with LogDNA logging
+
+## 🔨 Setup
+
+```bash
+npm i
+now deploy
+```
+
+Because of this app’s configuration, the values will only be injected by ZEIT
+Now on deploy (won’t run locally).
--- api/log.js
+++ api/log.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const helmet = require('helmet');
+const Logger = require('logdna');
+
+var logger = Logger.setupDefaultLogger(process.env.key);
 
 const app = express();
 
 app.use(helmet());
 
 app.get('*', (req, res) => {
-  console.log(process.env);
   res.set('Content-Type', 'text/html');
   res.status(200).send('Hi');
+});
+
+app.post('*', (req, res) => {
+  console.log(req);
+  logger.log(req);
 });
 
 module.exports = app;
